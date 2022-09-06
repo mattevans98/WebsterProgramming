a05de13239bbe4bcb144821b3151195b34acbd86
Matt Evans <mattevans98@gmail.com>
2022-09-06 14:35:25 -0500

    Added function prototypes

--- COSC1560/Homework/Assignment2/MovieStatistics.cpp
+++ COSC1560/Homework/Assignment2/MovieStatistics.cpp
@@ -1,7 +1,52 @@
+//*******************************************************************************************************
+//
+//		File:					MovieStatistics.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #A2
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					9/9/22
+//
+//
+//		This program gathers statistics about movies that college students see.
+//
+//		Other files required:
+//			1.	none
+//
+//*******************************************************************************************************
+
 #include <iostream>
+
+using namespace std;
+
+//*******************************************************************************************************
+
+void getAllData(int *, int);
+
+int getAverage(const int *, int);
+
+int getMin(const int *, int);
+
+void displayAllValue(const int *, int);
+
+void bubbleSort(int *, int);
+
+void swap(int *, int *);
+
+//*******************************************************************************************************
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	const int SIZE = 10;
+	int movies[SIZE];
+	int *ptr = movies;
+
 	return 0;
 }
+
+//*******************************************************************************************************
