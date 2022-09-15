e4b5e72d2d54b78b8e9a3ba52722eb599b9b5b67
Matt Evans <mattevans98@gmail.com>
2022-09-15 18:55:19 -0500

    initial commit

---
+++ COSC1560/Assignments/Assignment3/FlexibleArray.cpp
@@ -0,0 +1,41 @@
+//*******************************************************************************************************
+//
+//		File:					FlexibleArray.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #A3
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					9/16/22
+//
+//
+//		This program manipulates a dynamically allocated array using pointers
+//
+//		Other files required:
+//			1.	none
+//
+//*******************************************************************************************************
+
+int *getData(int);
+void display(const int *, int);
+void reverse(int *const, int);
+int *expand(int *, int);
+int *shift(int *, int);
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int size;
+	int *ptr;
+	cout << "Please enter a size: ";
+	cin >> size;
+
+	return 0;
+}
--- COSC1560/Homework/HW0912/HW0912.cpp
+++ COSC1560/Homework/HW0912/HW0912.cpp
@@ -20,15 +20,24 @@
 //
 //*******************************************************************************************************
 
+#include <iostream>
+using namespace  std;
+
 int *expand(int *, int);
 
 //*******************************************************************************************************
 
 int main()
 {
-	const int SIZE = 5;
+	const int SIZE = 5; // Size not constant
 	int *ptr = new int[SIZE]{3, 4, 5, 7, 9,};
 	ptr = expand(ptr, SIZE);
+
+	for (int i = 0; i < SIZE * 2; i++)
+	{
+		cout << ptr[i] << endl;
+	}
+
 	delete[] ptr;
 
 	return 0;
@@ -43,7 +52,7 @@ int *expand(int *ptr, int size)
 	newPtr = new int[newSize];
 
 	for (int i = 0; i < newSize; i++)
-		ptr[i] ? newPtr[i] = ptr[i] : newPtr[i] = 0;
+		i <  size ? newPtr[i] = ptr[i] : newPtr[i] = 0;
 
 	return newPtr;
 }
