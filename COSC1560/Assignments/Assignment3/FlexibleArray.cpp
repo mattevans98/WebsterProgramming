619ba85bfb4eee5352e8af9b8e5a2c9a3e2182f2
Matt Evans <mattevans98@gmail.com>
2022-09-15 20:08:10 -0500

    Added remaining display() logic

--- COSC1560/Assignments/Assignment3/FlexibleArray.cpp
+++ COSC1560/Assignments/Assignment3/FlexibleArray.cpp
@@ -26,16 +26,27 @@ using namespace std;
 
 //*******************************************************************************************************
 
+enum displayType
+{
+	INPUT,
+	REVERSED,
+	EXPANDED,
+	SHIFTED
+};
+
+//*******************************************************************************************************
+
 int *getData(int);
-void display(const int *, int);
+void display(const int *, int, displayType);
 void reverse(int *const, int);
 int *expand(int *, int &);
-int *shift(int *, int);
+int *shift(int *, int &);
 
 //*******************************************************************************************************
 
 int main()
 {
+	displayType type;
 	int size;
 	int *ptr = nullptr;
 
@@ -43,8 +54,11 @@ int main()
 	cin >> size;
 
 	ptr = getData(size);
-	display(ptr, size);
+	type = INPUT;
+	display(ptr, size, type);
 	ptr = expand(ptr, size);
+	type = EXPANDED;
+	display(ptr, size, type);
 
 	delete[] ptr;
 	return 0;
@@ -62,23 +76,42 @@ int *getData(int size)
 		cout << "Value " << i + 1 << ": ";
 		cin >> pArray[i];
 	}
+	cout << endl;
 
 	return pArray;
 }
 
 //*******************************************************************************************************
 
-void display(const int *ptr, int size)
+void display(const int *ptr, int size, displayType type)
 {
 	const int MAX_LINES = 5;
 
-	cout << "All values are:" << endl;
+	switch (type)
+	{
+		case INPUT:
+			cout << "All values are:" << endl;
+			break;
+		case REVERSED:
+			cout << "After reversing, all values are:" << endl;
+			break;
+		case EXPANDED:
+			cout << "After expanding, all values are:" << endl;
+			break;
+		case SHIFTED:
+			cout << "After shifting, all values are:" << endl;
+			break;
+		default:
+			cout << "Your switch statement is broken lol" << endl;
+	}
+
 	for (int i = 0; i < size; i++)
 	{
 		if (i == MAX_LINES)
 			cout << endl;
 		cout << left << setw(5) << ptr[i];
 	}
+	cout << endl;
 }
 
 //*******************************************************************************************************
@@ -89,7 +122,23 @@ int *expand(int *ptr, int &size)
 	int *pNewArr = new int[size];
 
 	for (int i = 0; i < size; i++)
-		pNewArr[i] = (i < size ? ptr[i] : 0);
+		pNewArr[i] = (i < (size / 2) ? ptr[i] : 0);
+
+	delete[] ptr;
+	return pNewArr;
+}
+
+//*******************************************************************************************************
+
+int *shift(int *ptr, int &size)
+{
+	const int FIRST_VAL = 100;
+	int newSize = size + 1;
+	int *pNewArr = new int[newSize];
+	pNewArr[0] = FIRST_VAL;
+
+	for (int i = 1; i < newSize; i++)
+		pNewArr[i] = ptr[i - 1];
 
 	delete[] ptr;
 	return pNewArr;
