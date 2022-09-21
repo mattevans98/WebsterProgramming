8a188bc67095c10cd1ca2c1755f7fc5dff0ab862
Matt Evans <mattevans98@gmail.com>
2022-09-21 12:31:06 -0500

    Initial commit

---
+++ COSC1560/Homework/HW0921/HW0921.cpp
@@ -0,0 +1,41 @@
+//*******************************************************************************************************
+//
+//		File:					HW0921.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #HW0921
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					9/23/2022
+//
+//
+//		This program answers the prompts on the assigned worksheet
+//
+//		Other files required:
+//			1.	none
+//
+//*******************************************************************************************************
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+//*******************************************************************************************************
+
+int main()
+{
+	const int SIZE = 81;
+	char *str = new char[SIZE];
+
+	// Problem #2
+	strcpy(str, "Hello");
+	cout << str << endl;
+
+
+}
+
+//*******************************************************************************************************
