f13c0ec54b45776f84760c06bd24f186ba74c8d7
Matt Evans <mattevans98@gmail.com>
2022-09-25 15:18:33 -0500

    Initial commit

---
+++ COSC1560/Assignments/Assignment4/PhoneBook.cpp
@@ -0,0 +1,28 @@
+//*******************************************************************************************************
+//
+//		File:					PhoneBook.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #A4
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					9/26/22
+//
+//
+//		This program stores 11 c-strings of phone numbers/names and searches through and displays them
+//
+//		Other files required:
+//			1.	none
+//
+//*******************************************************************************************************
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+//*******************************************************************************************************
+
