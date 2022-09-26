79b15a6700797664326f43f83b61d629213e729d
Matt Evans <mattevans98@gmail.com>
2022-09-25 21:31:00 -0500

    Initial commit

---
+++ COSC1560/Homework/HW0923/HW0923.cpp
@@ -0,0 +1,23 @@
+//*******************************************************************************************************
+//
+//		File:					HW0923.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #HW0923
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					9/26/22
+//
+//
+//		This program answers problem #8 on the c-string worksheet
+//
+//		Other files required:
+//			1.	none
+//
+//*******************************************************************************************************
+
+
