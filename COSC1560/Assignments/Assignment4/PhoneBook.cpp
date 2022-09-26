fdca6f779d5323d9dd32fe402ed768b56531f2e0
Matt Evans <mattevans98@gmail.com>
2022-09-25 20:51:11 -0500

    Fixed fill() function

--- COSC1560/Assignments/Assignment4/PhoneBook.cpp
+++ COSC1560/Assignments/Assignment4/PhoneBook.cpp
@@ -32,11 +32,11 @@ const int STRING_SIZE = 81;
 //*******************************************************************************************************
 
 void handler();
-void fill(char [][STRING_SIZE]);
-void search(char *[]);
-char *getShortest(char *[]);
-void enterBirthday(char *[]);
-void display(char *[]);
+void fill(char **);
+void search(char **);
+char getShortest(char [][STRING_SIZE]);
+void enterBirthday(char [][STRING_SIZE]);
+void display(char [][STRING_SIZE]);
 
 //*******************************************************************************************************
 
@@ -51,16 +51,34 @@ int main()
 
 void handler()
 {
+	//char phoneBook[SIZE][STRING_SIZE];
+	char **phoneBook = new char *[SIZE];
+	for (int i = 0; i < SIZE; ++i)
+		phoneBook[i] = new char[STRING_SIZE];
 
-	char phoneBook[SIZE][STRING_SIZE];
 	fill(phoneBook);
+	search(phoneBook);
 }
 
 //*******************************************************************************************************
 
-void fill(char phoneBook[][STRING_SIZE])
+void fill(char **phoneBook)
 {
-	char namesNums[SIZE][STRING_SIZE] = {
+	/*char ** pNamesNums = new char*[SIZE] {
+			"Alejandra Cruz, 555-1223",
+			"Joe looney, 555-0097",
+			"Geri Palmer, 555-8787",
+			"Li Chen, 555-1212",
+			"Holly Gaddis, 555-8878",
+			"Sam Wiggins, 555-0998",
+			"Bob Kain, 555-8712",
+			"Tim Haynes, 555-7676",
+			"Warren Gaddis, 555-9037",
+			"Jean James, 555-4939",
+			"Rom Palmer, 555-2783"
+	};*/
+
+	char namesNums[][STRING_SIZE] = {
 			"Alejandra Cruz, 555-1223",
 			"Joe looney, 555-0097",
 			"Geri Palmer, 555-8787",
@@ -75,11 +93,40 @@ void fill(char phoneBook[][STRING_SIZE])
 	};
 
 	for (int i = 0; i < SIZE; ++i)
-	{
 		strcpy(phoneBook[i], namesNums[i]);
-		cout << "phoneBook[" << i << "]: " << phoneBook[i] << endl;
-	}
 
 }
 
 //*******************************************************************************************************
+
+void search(char **phoneBook)
+{
+	char *pInput = new char[STRING_SIZE];
+	char **pFound = new char *[SIZE];
+	char *ptr = nullptr;
+	int matchNum = 0;
+
+	cout << "Enter a name or partial name to search for: ";
+	cin.getline(pInput, STRING_SIZE);
+	cout << endl;
+
+	cout << "Here are the results of the search: " << endl;
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		ptr = strstr(phoneBook[i], pInput);
+
+		if (ptr != nullptr)
+		{
+			pFound[i] = phoneBook[i];
+			cout << pFound[i] << endl;
+			matchNum++;
+		}
+	}
+
+	if (matchNum == 0)
+		cout << "No entries were found for " << pInput << endl;
+}
+
+//*******************************************************************************************************
+
