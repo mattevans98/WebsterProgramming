1186a0411f2e88da5b7882412e4a0c00bc156285
Matt Evans <mattevans98@gmail.com>
2022-10-02 14:56:35 -0500

    Initial commit

---
+++ COSC1560/Assignments/Assignment5/PigLatin.cpp
@@ -0,0 +1,43 @@
+//*******************************************************************************************************
+//
+//		File:					PigLatin.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #A5
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					10/3/22
+//
+//
+//		This program manipulates a string by translating it to pig latin
+//
+//		Other files required:
+//			1.	none
+//
+//*******************************************************************************************************
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//*******************************************************************************************************
+
+bool check_valid(const string &);
+void convert_to_upper(string &);
+void reverse(string &);
+void find_remove_gh(string &);
+void append_ay(string &);
+void append_length(string &);
+
+//*******************************************************************************************************
+
+int main()
+{
+	return 0;
+}
+
+//*******************************************************************************************************
