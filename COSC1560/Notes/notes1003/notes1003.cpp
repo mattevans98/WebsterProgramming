030d2b7f3876be990866b6e4197f227fe4ff2857
Matt Evans <mattevans98@gmail.com>
2022-10-03 10:44:21 -0500

    Added sum() and capitalize()

--- COSC1560/Notes/notes1003/notes1003.cpp
+++ COSC1560/Notes/notes1003/notes1003.cpp
@@ -2,20 +2,29 @@
 using namespace std;
 
 void capitalize(char *);
+int sum(char *);
 
 int main()
 {
 	const int SIZE = 81;
-	char *str;
+	char *str = new char[81];
 	strcpy(str, "hello. my name is joe. what is your name.");
 
 	capitalize(str);
+	cout << str << endl;
 
+	strcpy(str, "251x3w");
+	cout << sum(str) << endl;
+
+	delete[] str;
 	return 0;
 }
 
 void capitalize(char *str)
 {
+	//char * s = strstr(str, ". ");
+	//str = s;
+
 	*str = toupper(*str);
 	str++;
 
@@ -24,6 +33,27 @@ void capitalize(char *str)
 		if (*str == '.' && *(str + 1) == ' ')
 		{
 			*(str + 2) = toupper(*(str + 2));
+			str += 2;
+		}
+		else
+		{
+			str++;
 		}
 	}
+}
+
+int sum(char *str)
+{
+	int total = 0;
+	while (*str != '\0')
+	{
+		if (isdigit(*str))
+		{
+			total += (*str - '0');
+		}
+
+		str++;
+	}
+
+	return total;
 }
