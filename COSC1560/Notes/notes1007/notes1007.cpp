8d0fdb45c3e0cd0202f723106667d4ace13fc35e
Matt Evans <mattevans98@gmail.com>
2022-10-07 10:35:11 -0500

    Added notes

--- COSC1560/Notes/notes1007/notes1007.cpp
+++ COSC1560/Notes/notes1007/notes1007.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
 using namespace std;
 
+//*******************************************************************************************************
+
+/*
+Primitive Data Types
+	- int/char/double etc.
+
+Abstract Data Types (ADT)
+ 	- struct
+ 		- Should only contain member variables
+*/
+
+//*******************************************************************************************************
+
+struct Rectangle
+{
+	int width;
+	int length;
+};
+
 int main()
 {
+	Rectangle floor;
+	floor.width = 5;
+	floor.length = 10;
+
+	int area = floor.length * floor.width;
+
+	Rectangle carpet = {10, 30};
+
+	Rectangle myRecs[5] = {
+			{0, 0},
+			{0, 1},
+			{0, 2},
+			{0, 3},
+			{0, 4}
+	};
+
 	return 0;
 }
