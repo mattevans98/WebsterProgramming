1ea4e1c45461f0ea5f5c50d37194d16de563fc7b
Matt Evans <mattevans98@gmail.com>
2022-10-09 22:30:37 -0500

    Initial commit

---
+++ COSC1560/Homework/HW1007/HW1007.cpp
@@ -0,0 +1,25 @@
+//*******************************************************************************************************
+//
+//		File:					HW1007.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #HW1007
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					10/10/22
+//
+//
+//		This program answers questions 11.1 and 11.2 in the textbook
+//
+//		Other files required:
+//			1.	none
+//
+//*******************************************************************************************************
+
+#include <iostream>
+using namespace std;
+
