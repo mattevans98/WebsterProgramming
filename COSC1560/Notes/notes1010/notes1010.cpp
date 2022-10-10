b04fe6e2fe79674a52b8b035c757bdab1a8d114a
Matt Evans <mattevans98@gmail.com>
2022-10-10 10:24:47 -0500

    Added struct and getInfo()

--- COSC1560/Notes/notes1010/notes1010.cpp
+++ COSC1560/Notes/notes1010/notes1010.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
+struct Employee
+{
+    string name;
+    int hours;
+    double hourly;
+};
+
+Employee getInfo();
+
 int main()
 {
+    getInfo();
+
     return 0;
+}
+
+Employee getInfo()
+{
+    Employee emp;
+
+    getline(cin, emp.name);
+    cin >> emp.hours;
+    cin >> emp.hourly;
+
+    return emp;
 }
