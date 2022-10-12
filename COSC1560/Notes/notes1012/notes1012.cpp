2d842904ea409faf68044fe6214c4937a724c8fb
Matt Evans <mattevans98@gmail.com>
2022-10-12 10:20:24 -0500

    Added getInfo() and display()

--- COSC1560/Notes/notes1012/notes1012.cpp
+++ COSC1560/Notes/notes1012/notes1012.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//*******************************************************************************************************
+
 struct Date
 {
     int month;
@@ -17,21 +19,56 @@ struct Address
 struct Employee
 {
     string name;
-    int id;
     Date birthday;
     Address homeAddress;
 };
 
+//*******************************************************************************************************
+
 void getInfo(Employee &);
+void display(const Employee [], int);
+
+//*******************************************************************************************************
 
 int main()
 {
     Employee emp;
+    getInfo(emp);
+    Employee emps[5];
+
+    for (int i = 0; i < 5; i++)
+        getInfo(emps[i]);
+
+    display(emps, 5);
 
     return 0;
 }
 
+//*******************************************************************************************************
+
 void getInfo(Employee &emp)
 {
-
+    getline(cin, emp.name);
+    getline(cin, emp.homeAddress.street);
+    cin >> emp.homeAddress.zip;
+    cin >> emp.birthday.month;
+    cin >> emp.birthday.day;
+    cin >> emp.birthday.year;
 }
+
+//*******************************************************************************************************
+
+void display(const Employee emps[], int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        cout << emps[i].name << endl;
+        cout << emps[i].homeAddress.street << endl;
+        cout << emps[i].homeAddress.zip << endl;
+        cout << emps[i].birthday.month << endl;
+        cout << emps[i].birthday.day << endl;
+        cout << emps[i].birthday.year << endl;
+    }
+}
+
+//*******************************************************************************************************
