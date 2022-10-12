a17d315ffff32a21e308337f372f1a7b863881de
Matt Evans <mattevans98@gmail.com>
2022-10-11 21:31:17 -0500

    Added handler() logic

--- COSC1560/Assignments/Assignment6/StudentInfo.cpp
+++ COSC1560/Assignments/Assignment6/StudentInfo.cpp
@@ -52,8 +52,58 @@ int main()
 
 void handler()
 {
+    const int NUM_STARS = 81;
     const int SIZE = 10;
-    Students students[SIZE];
+    int choice;
+    Students students[SIZE] = {
+            {"-", 0, 0},
+            {"-", 0, 0},
+            {"-", 0, 0},
+            {"-", 0, 0},
+            {"-", 0, 0},
+            {"-", 0, 0},
+            {"-", 0, 0},
+            {"-", 0, 0},
+            {"-", 0, 0},
+            {"-", 0, 0}
+    };
+
+    for (int i = 0; i < NUM_STARS; ++i)
+        cout << "*";
+
+    cout << "1) Edit a student" << endl;
+    cout << "2) View student's info" << endl;
+    cout << "3) Display a count of how many students are in the list." << endl;
+    cout << "4) Delete a student" << endl;
+    cout << "5) Quit" << endl;
+    cin >> choice;
+
+    switch (choice)
+    {
+        case 1:
+            editStudents(students, SIZE);
+            break;
+        case 2:
+            viewStudent(students, SIZE);
+            break;
+        case 3:
+            countStudent(students, SIZE);
+            break;
+        case 4:
+            deleteStudent(students, SIZE);
+            break;
+        case 5:
+            cout << "Finished!" << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
+            handler();
+    }
 }
 
 //*******************************************************************************************************
+
+void editStudent(Students students[], int size)
+{
+    int indexNum;
+}
