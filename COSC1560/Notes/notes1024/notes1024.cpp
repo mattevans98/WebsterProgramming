c923e6f629d59a519673ddb07d2bf056f46373dd
Matt Evans <mattevans98@gmail.com>
2022-10-24 10:43:49 -0500

    Added loops

--- COSC1560/Notes/notes1024/notes1024.cpp
+++ COSC1560/Notes/notes1024/notes1024.cpp
@@ -22,16 +22,30 @@ int main()
 {
     Town t = {"Webster Groves", 10000};
     County c;
-    County *ptr;
+    County *ptr = new County[5];
 
     cin >> c.numTowns;
     c.ptr = new Town[c.numTowns];
     for (int i = 0; i < c.numTowns; i++)
-    {
         cin >> c.ptr[i].population;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "County: " << i + 1;
+        cin >> (ptr + i)->numTowns;
+        ptr[i].ptr = new Town[ptr[i].numTowns];
+        for (int j = 0; j < ptr[i].numTowns; j++)
+        {
+            cin.ignore();
+            getline(cin, ptr[i].ptr[j].name);
+            cin >> ptr[i].ptr[j].population;
+        }
     }
 
     delete[] c.ptr;
+    for (int i = 0; i < 5; i++)
+        delete[] ptr[i].ptr;
+    delete[] ptr;
     return 0;
 }
 
