96f1f5b815ede90127de5edbbdcff5c10a442ce4
Matt Evans <mattevans98@gmail.com>
2022-10-24 10:22:01 -0500

    Added structs and main()

--- COSC1560/Notes/notes1024/notes1024.cpp
+++ COSC1560/Notes/notes1024/notes1024.cpp
@@ -3,3 +3,37 @@ using namespace std;
 
 //*******************************************************************************************************
 
+struct Town
+{
+    string name;
+    double population;
+};
+
+struct County
+{
+    int numTowns;
+    Town *ptr;
+    double avgPopulation;
+};
+
+//*******************************************************************************************************
+
+int main()
+{
+    Town t = {"Webster Groves", 10000};
+    County c;
+    County *ptr;
+
+    cin >> c.numTowns;
+    c.ptr = new Town[c.numTowns];
+    for (int i = 0; i < c.numTowns; i++)
+    {
+        cin >> c.ptr[i].population;
+    }
+
+    delete[] c.ptr;
+    return 0;
+}
+
+//*******************************************************************************************************
+
