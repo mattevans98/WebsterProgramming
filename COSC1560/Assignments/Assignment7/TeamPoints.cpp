c290bc77143df4f46d833df25243be9424fb16b4
Matt Evans <mattevans98@gmail.com>
2022-10-25 12:53:41 -0500

    Initial commit

---
+++ COSC1560/Assignments/Assignment7/TeamPoints.cpp
@@ -0,0 +1,22 @@
+//*******************************************************************************************************
+//
+//		File:					TeamPoints.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #A7
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					10/26/22
+//
+//
+//		This program keeps track of a team's information
+//
+//		Other files required:
+//			1.	none
+//
+//*******************************************************************************************************
+
