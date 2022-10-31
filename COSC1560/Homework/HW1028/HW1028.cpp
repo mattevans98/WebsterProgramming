c3a554ebd337709923e1659bab2589c93a1bbfa1
Matt Evans <mattevans98@gmail.com>
2022-10-31 09:58:24 -0500

    Initial commit

---
+++ COSC1560/Homework/HW1028/HW1028.cpp
@@ -0,0 +1,28 @@
+//*******************************************************************************************************
+//
+//		File:					HW1028.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #HW1028
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					10/26/22
+//
+//
+//		This program completes the copyToFile function from assignment 8
+//
+//		Other files required:
+//			1.	none
+//
+//*******************************************************************************************************
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//*******************************************************************************************************
+
