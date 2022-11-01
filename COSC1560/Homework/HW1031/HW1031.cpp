504ff4c28f40a3143014471fb7457efd4db2a867
Matt Evans <mattevans98@gmail.com>
2022-11-01 13:59:32 -0500

    Initial commit

---
+++ COSC1560/Homework/HW1031/HW1031.cpp
@@ -0,0 +1,28 @@
+//*******************************************************************************************************
+//
+//		File:					HW1031.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #HW1031
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					11/2/22
+//
+//
+//		This program completes the displayLastFive() function from assignment 8
+//
+//		Other files required:
+//			1.	none
+//
+//*******************************************************************************************************
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//*******************************************************************************************************
+
