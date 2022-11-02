2698a4413e003e5e0ffeabc8f4a35198e4de354b
Matt Evans <mattevans98@gmail.com>
2022-11-02 10:23:51 -0500

    Added write

--- COSC1560/Notes/notes1102/notes1102.cpp
+++ COSC1560/Notes/notes1102/notes1102.cpp
@@ -10,8 +10,16 @@ int main()
             SIZE2 = 5;
     int a = 100;
     char letter = 'A';
+
+    file.write(reinterpret_cast<char *>(&a), sizeof(a));
+    file.write(&letter, sizeof(letter));
+
     char msg[SIZE] = "Goodbye Cruel World!";
+    file.write(msg, sizeof(msg));
+
     double vals[SIZE2] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    file.write(reinterpret_cast<char *>(vals), sizeof(vals));
+    file.close();
 
     return 0;
 }
