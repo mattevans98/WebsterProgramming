326bc1f69708b3f88c08a1934af9a8217757fb89
Matt Evans <mattevans98@gmail.com>
2022-11-02 10:45:57 -0500

    Added reading

--- COSC1560/Notes/notes1102/notes1102.cpp
+++ COSC1560/Notes/notes1102/notes1102.cpp
@@ -1,10 +1,11 @@
 #include <fstream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     fstream file;
-    file.open("record.dat", ios::in | ios::out | ios::binary);
+    file.open("record.dat", ios::out | ios::binary);
 
     const int SIZE = 81,
             SIZE2 = 5;
@@ -12,14 +13,40 @@ int main()
     char letter = 'A';
 
     file.write(reinterpret_cast<char *>(&a), sizeof(a));
-    file.write(&letter, sizeof(letter));
+    file.write(&letter, sizeof(letter)); // reinterpret_cast not needed bc it's already a char
 
     char msg[SIZE] = "Goodbye Cruel World!";
-    file.write(msg, sizeof(msg));
+    file.write(msg, sizeof(msg)); // reinterpret_cast not needed bc it's already a *char
 
     double vals[SIZE2] = {1.1, 2.2, 3.3, 4.4, 5.5};
     file.write(reinterpret_cast<char *>(vals), sizeof(vals));
     file.close();
+
+    file.open("record.dat", ios::in | ios::binary);
+    int a1;
+    char letter1;
+    char msg1[SIZE];
+    double vals1[SIZE2];
+
+    if (!file.fail())
+    {
+        file.read(reinterpret_cast<char *>(&a1), sizeof(a1));
+        cout << a1 << " ";
+
+        file.read(&letter1, sizeof(letter1));
+        cout << letter1 << " ";
+
+        file.read(msg1, sizeof(msg1));
+        cout << msg1 << endl;
+
+        file.read(reinterpret_cast<char *>(&vals1), sizeof(vals1));
+        for (double val1: vals1)
+            cout << val1 << endl;
+    }
+    else
+    {
+        cout << "Error opening file" << endl;
+    }
 
     return 0;
 }
