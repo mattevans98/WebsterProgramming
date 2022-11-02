e4d26c646317dc0b5742f280bde8c3ff9c3a839f
Matt Evans <mattevans98@gmail.com>
2022-11-02 10:10:41 -0500

    Added variables

--- COSC1560/Notes/notes1102/notes1102.cpp
+++ COSC1560/Notes/notes1102/notes1102.cpp
@@ -4,6 +4,14 @@ using namespace std;
 int main()
 {
     fstream file;
-    file.open("record.dat");
+    file.open("record.dat", ios::in | ios::out | ios::binary);
+
+    const int SIZE = 81,
+            SIZE2 = 5;
+    int a = 100;
+    char letter = 'A';
+    char msg[SIZE] = "Goodbye Cruel World!";
+    double vals[SIZE2] = {1.1, 2.2, 3.3, 4.4, 5.5};
+
     return 0;
 }
