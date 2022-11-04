a5627c2fd6df9898d96530af274b72c554253ab8
Matt Evans <mattevans98@gmail.com>
2022-11-03 21:47:31 -0500

    Added function prototypes

--- COSC1560/Assignments/Assignment8/NameReport.cpp
+++ COSC1560/Assignments/Assignment8/NameReport.cpp
@@ -1,3 +1,59 @@
+//*******************************************************************************************************
 //
-// Created by Matt Evans on 11/3/22.
+//		File:					NameReport.cpp
 //
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #A8
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					11/3/22
+//
+//
+//		This program keeps track of a team's information
+//
+//		Other files required:
+//			1.	names.txt
+//
+//*******************************************************************************************************
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//*******************************************************************************************************
+
+int getChoice();
+void handleMenu(ifstream &);
+void displayFirstFive(ifstream &);
+void displayLastFive(ifstream &);
+int count(ifstream &);
+void copyToFile(ifstream &, ofstream &);
+void search(ifstream &);
+
+//*******************************************************************************************************
+
+int main()
+{
+    ifstream inFile;
+    ofstream outFile;
+    inFile.open("names.txt");
+    outFile.open("names2.txt");
+    int choice;
+
+    do
+    {
+        choice = getChoice();
+        handleMenu(inFile);
+    } while (choice != 6);
+
+    inFile.close();
+    outFile.close();
+
+    return 0;
+}
+
+//*******************************************************************************************************
