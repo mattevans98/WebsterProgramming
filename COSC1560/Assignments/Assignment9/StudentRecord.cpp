be2e9228debb7f4642c3d044aba5c745198fe3d7
Matt Evans <mattevans98@gmail.com>
2022-11-07 10:05:28 -0600

    Initial commit

---
+++ COSC1560/Assignments/Assignment9/StudentRecord.cpp
@@ -0,0 +1,21 @@
+//*******************************************************************************************************
+//
+//		File:					StudentRecord.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #A9
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					11/11/22
+//
+//
+//		This program reads and writes student records to a file
+//
+//		Other files required:
+//			1.	record.dat
+//
+//*******************************************************************************************************
