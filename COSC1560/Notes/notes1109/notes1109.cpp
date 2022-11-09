246144e41eab099269909c0df0eebd2398658a50
Matt Evans <mattevans98@gmail.com>
2022-11-09 10:24:07 -0600

    Added notes

--- COSC1560/Notes/notes1109/notes1109.cpp
+++ COSC1560/Notes/notes1109/notes1109.cpp
@@ -14,5 +14,25 @@ int main()
     long size = sizeof(a);
     dataFile.seekp(6 * size, ios::beg);
     dataFile.write(reinterpret_cast<char *>(&a), size); // Replaces 6 with 21
+
+    a = 22;
+    dataFile.seekp(-size, ios::cur);
+    dataFile.write(reinterpret_cast<char *>(&a), size); // Replaces 6 with 22
+
+    cout << dataFile.tellp() << endl; // Outputs: 28
+
+    a = 23;
+    dataFile.seekp(-4 * size, ios::end);
+    dataFile.write(reinterpret_cast<char *>(&a), size); // Replaces 7 with 23
+
+    a = 24;
+    dataFile.seekp(0L, ios::beg);
+    dataFile.write(reinterpret_cast<char *>(&a), size); // Replaces 0 with 24
+
+    a = 25;
+    dataFile.seekp(0L, ios::end);
+    dataFile.write(reinterpret_cast<char *>(&a), size); // Appends 25 to end of file
+
+    return 0;
 }
 
