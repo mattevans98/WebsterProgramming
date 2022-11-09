83acec7dd8aa63d651472d5f27d98be2c63b7397
Matt Evans <mattevans98@gmail.com>
2022-11-09 14:34:08 -0600

    Added validateInput() and getChoice()

--- COSC1560/Assignments/Assignment9/StudentRecord.cpp
+++ COSC1560/Assignments/Assignment9/StudentRecord.cpp
@@ -27,6 +27,9 @@ using namespace std;
 
 //*******************************************************************************************************
 
+template<typename T>
+bool validateInput(const T &, T, T);
+
 const int STRING_SIZE = 51;
 
 //*******************************************************************************************************
@@ -58,8 +61,21 @@ int main()
 
 //*******************************************************************************************************
 
+template<typename T>
+bool validateInput(const T &input, const T MIN, const T MAX)
+{
+    bool isValid;
+    if (input < MIN || input > MAX)
+        isValid = false;
+    else
+        isValid = true;
+
+    return isValid;
+}
+
 void handleMenu()
 {
+    int choice;
     cout << string(STRING_SIZE, '*') << endl;
     cout << setw(5) << left << "1." << "Enter new records into the file." << endl;
     cout << setw(5) << "2." << setw(5) <<
@@ -68,6 +84,27 @@ void handleMenu()
     cout << setw(5) << "4." << setw(5) << "Display all records." << endl;
     cout << setw(5) << "5." << setw(5) << "Quit." << endl;
     cout << endl;
+
+    choice = getChoice();
+}
+
+//*******************************************************************************************************
+
+int getChoice()
+{
+    const int MIN = 1, MAX = 5;
+    int choice;
+
+    cout << "Please enter: ";
+    cin >> choice;
+
+    while (!validateInput(choice, MIN, MAX))
+    {
+        cout << "Invalid input: Enter again: ";
+        cin >> choice;
+    }
+
+    return choice;
 }
 
 //*******************************************************************************************************
