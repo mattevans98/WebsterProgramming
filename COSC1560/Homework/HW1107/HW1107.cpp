d3a62edb6856d5fa7832bb3fd8f826adf7b4f4b5
Matt Evans <mattevans98@gmail.com>
2022-11-09 12:38:55 -0600

    Added answer

--- COSC1560/Homework/HW1107/HW1107.cpp
+++ COSC1560/Homework/HW1107/HW1107.cpp
@@ -26,28 +26,30 @@ using namespace std;
 
 //*******************************************************************************************************
 
+// File Contents: 0 1 2 3 4 5 6 7 8 9 10 <EOF>
+
 int main()
 {
-    fstream dataFile("record.dat", ios::out | ios::binary);
+    fstream d;
+    d.open("record.dat", ios::binary | ios::in | ios::out);
+    int a;
+    long size = sizeof(a);
 
-    for (int i = 0; i <= 10; i++)
-    {
-        dataFile.write(reinterpret_cast<char *>(&i), sizeof(i));
-    }
+    d.seekg(2 * size, ios::cur);
+    d.read(reinterpret_cast<char *>(&a), size);
+    cout << a; // Outputs 2
 
-    dataFile.close();
+    d.seekg(-size, ios::cur);
+    d.read(reinterpret_cast<char *>(&a), size);
+    cout << a; // Outputs 9
+
+    d.seekg(5 * size, ios::beg);
+    d.read(reinterpret_cast<char *>(&a), size);
+    cout << a; // Outputs 5
+
+    d.seekg(-3 * size, ios::cur);
+    d.read(reinterpret_cast<char *>(&a), size);
+    cout << a << endl; // Outputs 3
 
     return 0;
-//    fstream d;
-//    d.open("record.dat", ios::binary | ios::in | ios::out);
-//    int a;
-//    long size = sizeof(a);
-//
-//    d.seekg(2 * size, ios::cur);
-//    d.read(reinterpret_cast<char *>(&a), size);
-//    cout << a; // Outputs 2
-//
-//    d.seekg(-size, ios::cur);
-//    d.read(reinterpret_cast<char *>(&a), size);
-//    cout << a; // Outputs 9
 }
