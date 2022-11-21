01b58307f4d9e0f0734ea818f5ce011eca15a859
Matt Evans <mattevans98@gmail.com>
2022-11-20 19:20:15 -0600

    Initial commit

---
+++ COSC1560/Assignments/Assignment10/Employee.cpp
@@ -0,0 +1,22 @@
+//*******************************************************************************************************
+//
+//		File:					Employee.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #A10
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					11/21/22
+//
+//
+//		his program takes input and displays output of data stored in Employee objects
+//
+//		Other files required:
+//			1.	EmployeeDriver.cpp
+//          2.  Employee.h
+//
+//*******************************************************************************************************
---
+++ COSC1560/Assignments/Assignment10/Employee.h
@@ -0,0 +1,27 @@
+//*******************************************************************************************************
+//
+//		File:					Employee.h
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #A10
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					11/21/22
+//
+//
+//		This program takes input and displays output of data stored in Employee objects
+//
+//		Other files required:
+//			1.	Employee.cpp
+//          2.  EmployeeDriver.cpp
+//
+//*******************************************************************************************************
+
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#endif
