e7790a22d0c1ea1edaeabdfc9b35885a11e3dc23
Matt Evans <mattevans98@gmail.com>
2022-11-27 15:26:03 -0600

    Added program boilerplate

--- COSC1560/Homework/HW1123/HW1123.cpp
+++ COSC1560/Homework/HW1123/HW1123.cpp
@@ -13,10 +13,44 @@
 //		Due:					11/28/22
 //
 //
-//		This program completes the program from problem 11.26 in the textbook
+//		This program completes the program from problem 13.26 in the textbook
 //
 //		Other files required:
 //			1.	none
 //
 //*******************************************************************************************************
 
+#include <iostream>
+using namespace std;
+
+//*******************************************************************************************************
+
+class Yard
+{
+private:
+    int length;
+    int width;
+public:
+    Yard()
+    {
+        length = 0;
+        width = 0;
+    }
+
+    void setLength(int len)
+    {
+        length = len;
+    }
+
+    void setWidth(int w)
+    {
+        width = w;
+    }
+};
+
+//*******************************************************************************************************
+
+int main()
+{
+
+}
