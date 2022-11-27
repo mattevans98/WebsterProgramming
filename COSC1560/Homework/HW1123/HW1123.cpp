d633d624d0d0a370f27b27f3b296bde998f70cd5
Matt Evans <mattevans98@gmail.com>
2022-11-27 15:23:25 -0600

    Initial commit

---
+++ COSC1560/Homework/HW1123/HW1123.cpp
@@ -0,0 +1,22 @@
+//*******************************************************************************************************
+//
+//		File:					HW1123.cpp
+//
+//		Student:				Matt Evans
+//
+//		Assignment:				Program #HW1123
+//
+//		Course Name:			Programming II
+//
+//		Course Number:			COSC 1560-01
+//
+//		Due:					11/28/22
+//
+//
+//		This program completes the program from problem 11.26 in the textbook
+//
+//		Other files required:
+//			1.	none
+//
+//*******************************************************************************************************
+
