05e086a975373753a042c38d016d3aaba5751ddb
Matt Evans <mattevans98@gmail.com>
2022-11-29 10:37:43 -0600

    Added member function definitions

--- COSC1560/Assignments/Assignment11/Student.cpp
+++ COSC1560/Assignments/Assignment11/Student.cpp
@@ -22,3 +22,75 @@
 //*******************************************************************************************************
 
 #include "Student.h"
+
+//*******************************************************************************************************
+
+Student::Student()
+{
+    studentId = 0;
+    strcpy(studentName, "*none*");
+    gpa = 0.0;
+}
+
+//*******************************************************************************************************
+
+Student::Student(int studentId, char *studentName, double gpa)
+{
+    this->studentId = studentId;
+    this->studentName = new char[strlen(studentName) + 1];
+    strcpy(this->studentName, studentName);
+    this->gpa = gpa;
+}
+
+//*******************************************************************************************************
+
+Student::~Student()
+{
+    delete[] studentName;
+}
+
+//*******************************************************************************************************
+
+void Student::setStudentId(int studentId)
+{
+    this->studentId = studentId;
+}
+
+//*******************************************************************************************************
+
+void Student::setStudentName(char *studentName)
+{
+    delete[] this->studentName;
+    this->studentName = new char[strlen(studentName) + 1];
+    strcpy(this->studentName, studentName);
+}
+
+//*******************************************************************************************************
+
+void Student::setGpa(double gpa)
+{
+    this->gpa = gpa;
+}
+
+//*******************************************************************************************************
+
+int Student::getStudentId() const
+{
+    return studentId;
+}
+
+//*******************************************************************************************************
+
+char *Student::getStudentName() const
+{
+    return studentName;
+}
+
+//*******************************************************************************************************
+
+double Student::getGpa() const
+{
+    return gpa;
+}
+
+//*******************************************************************************************************
